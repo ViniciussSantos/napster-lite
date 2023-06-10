06fcf59bfcab988499119e722f2d9036654d4ea0
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-06-10 16:12:53 -0300

    adding file transfer between peers

--- Peer.java
+++ Peer.java
@@ -19,7 +19,7 @@ public class Peer {
         String[] files = FileHelper.getFilesInFolder(folderPath);
 
         // Start the server thread to listen for incoming connections from other peers
-        new ServerThread(port).start();
+        new ServerThread(port, folderPath).start();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -28,17 +28,18 @@ public class Peer {
             String command = scanner.nextLine();
             String[] commandParts = command.split(" ");
 
-            switch (commandParts[0]) {
+            switch (commandParts[0].trim()) {
                 case "SEARCH":
                     System.out.println("NOT IMPLEMENTED YET");
                     break;
                 case "DOWNLOAD":
-                    if (commandParts.length != 2) {
+                    if (commandParts.length != 3) {
                         System.out.println("Invalid command");
                         break;
                     }
-                    String peerPort = commandParts[1];
-                    new ClientThread(Integer.parseInt(peerPort)).start();
+                    String peerPort = commandParts[1].split(":")[1].trim();
+                    String fileName = commandParts[2].trim();
+                    new ClientThread(Integer.parseInt(peerPort), folderPath, fileName).start();
                     break;
                 default:
                     System.out.println("Invalid command");
@@ -55,9 +56,12 @@ public class Peer {
      */
     private static class ServerThread extends Thread {
         private final int port;
+        private final String folderPath;
 
-        public ServerThread(int port) {
+
+        public ServerThread(int port, String folderPath) {
             this.port = port;
+            this.folderPath = folderPath;
         }
 
         @Override
@@ -68,9 +72,7 @@ public class Peer {
 
                 while (!Thread.currentThread().isInterrupted()) {
                     Socket clientSocket = serverSocket.accept();
-                    System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
-
-                    new Thread(new ServerHandler(clientSocket)).start();
+                    new Thread(new ServerHandler(clientSocket, folderPath)).start();
                 }
 
                 serverSocket.close();
@@ -85,20 +87,54 @@ public class Peer {
      */
     private static class ServerHandler implements Runnable {
         private final Socket clientSocket;
+        private final String filePath;
 
-        public ServerHandler(Socket clientSocket) {
+
+        public ServerHandler(Socket clientSocket, String filePath) {
             this.clientSocket = clientSocket;
+            this.filePath = filePath;
         }
 
         @Override
         public void run() {
             try {
-                //TODO: Add the code to handle the download requests from other peers
-                //This is just a sample code to read the message sent by the client
-                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String input = br.readLine();
-                System.out.println("Received message from client: " + input);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+
+                String request = reader.readLine();
+                String[] requestParts = request.split(" ");
+
+                if (requestParts.length != 2 || !requestParts[0].equals("DOWNLOAD")) {
+                    writer.println("Invalid request");
+                    clientSocket.close();
+                    return;
+                }
+
+                String fileName = requestParts[1];
+                System.out.println("File name: " + fileName);
+
+                if (!FileHelper.checkIfFileExists(filePath, fileName)) {
+                    writer.println("File not found");
+                    clientSocket.close();
+                    return;
+                }
+                File file = new File(filePath + "/" + fileName);
+                FileInputStream fis = new FileInputStream(file);
+                OutputStream os = clientSocket.getOutputStream();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+
+                os.flush();
+                os.close();
+                fis.close();
                 clientSocket.close();
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -110,22 +146,46 @@ public class Peer {
      */
     private static class ClientThread extends Thread {
         private final int port;
+        private final String folderPath;
+        private final String fileName;
 
-        public ClientThread(int port) {
+        public ClientThread(int port, String folderPath, String fileName) {
             this.port = port;
+            this.folderPath = folderPath;
+            this.fileName = fileName;
         }
 
         @Override
         public void run() {
 
             try {
+                if (FileHelper.checkIfFileExists(folderPath, fileName)) {
+                    System.out.println("File already exists in this peer");
+                    return;
+                }
+
                 String serverAddress = "localhost";
                 Socket socket = new Socket(serverAddress, port);
                 System.out.println("Connected to server: " + socket.getInetAddress().getHostAddress());
 
-                OutputStream os = socket.getOutputStream();
-                DataOutputStream serverWriter = new DataOutputStream(os);
-                serverWriter.writeUTF("Hello from client " + socket.getLocalSocketAddress() + "\n");
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                writer.println("DOWNLOAD " + fileName);
+
+                File file = new File(folderPath + "/" + fileName);
+
+                FileOutputStream fos = new FileOutputStream(file);
+                InputStream is = socket.getInputStream();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+
+                fos.close();
+                is.close();
+
+                System.out.println("File received: " + folderPath);
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
