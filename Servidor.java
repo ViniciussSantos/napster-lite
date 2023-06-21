91ec83099c6d167fbda327d4db48e174728f96a7
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-06-20 21:38:11 -0300

    Added init args for the server

--- Servidor.java
+++ Servidor.java
@@ -6,10 +6,26 @@ import java.rmi.registry.Registry;
 public class Servidor {
     public static void main(String[] args) throws Exception {
 
-        LocateRegistry.createRegistry(1099);
+        String ipAddress = "127.0.0.1";
+        String port = "1099";
+
+        if (args.length != 2) {
+            System.out.println("Usage: java Servidor <ip> <port>");
+        }
+
+        if (args.length == 1) {
+            ipAddress = args[0];
+        }
+
+        if (args.length == 2) {
+            ipAddress = args[0];
+            port = args[1];
+        }
+
+        LocateRegistry.createRegistry(Integer.parseInt(port));
         Registry registry = LocateRegistry.getRegistry();
-        ServerServiceImpl peerService = new ServerServiceImpl();
-        registry.bind("rmi://127.0.0.1/ServerService", peerService);
+        ServerServiceImpl serverService = new ServerServiceImpl();
+        registry.bind("rmi://" + ipAddress + "/ServerService", serverService);
         System.out.println("Servidor iniciado");
     }
 }
